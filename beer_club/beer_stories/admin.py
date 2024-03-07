e1fd3f100fc24f0147ebad67e5c916b67952c58a
Your Name <lukasjonaitisltu@gmail.com>
2024-03-07 11:53:24 +0200

    Iki admino ir appso.

---
+++ beer_club/beer_stories/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.Types)
+admin.site.register(models.Review)
---
+++ beer_club/beer_stories/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class BeerStoriesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'beer_stories'
+
+class Meta: 
+    verbose_name = _('beer_stories')
