035217c8c6b7c10c2062c28b5ca5c7706e48321b
Your Name <lukasjonaitisltu@gmail.com>
2024-03-22 11:04:00 +0200

    Front-footer

--- beer_club/beer_stories/views.py
+++ beer_club/beer_stories/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils.translation import gettext_lazy as _
 from . import models
 
 
