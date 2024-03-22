781581039616772519b883978482cf45662de36a
Your Name <lukasjonaitisltu@gmail.com>
2024-03-22 13:35:42 +0200

    Userio registro templetitas

--- beer_club/users/views.py
+++ beer_club/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.utils.translation import gettext_lazy as _
+from . import forms
 
-# Create your views here.
+def signup(request:HttpRequest) -> HttpResponse:
+    form = forms.CreateUserForm()
+    return render(request, 'users/signup.html', {
+        'form': form,
+    })
