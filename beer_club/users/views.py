8a26691591f904a07eea3097e9f284242cc78324
Your Name <lukasjonaitisltu@gmail.com>
2024-03-22 15:34:48 +0200

    USER signup. Truksta logino templeito

--- beer_club/users/views.py
+++ beer_club/users/views.py
@@ -5,7 +5,14 @@ from django.utils.translation import gettext_lazy as _
 from . import forms
 
 def signup(request:HttpRequest) -> HttpResponse:
-    form = forms.CreateUserForm()
+    if request.method == "POST":
+        form= forms.CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, _("Thank you! You successfully joined BeerClub."))
+            return redirect('index')
+    else:
+        form = forms.CreateUserForm()
     return render(request, 'users/signup.html', {
         'form': form,
     })
