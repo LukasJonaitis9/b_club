15a51e2ee6edeef182102d7682fd931c90afcc78
Your Name <lukasjonaitisltu@gmail.com>
2024-03-25 13:13:20 +0200

    Login / singout templeitas

--- beer_club/users/urls.py
+++ beer_club/users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('detail/', views.user_detail, name = 'user_detail_current'),
+    path('detail/<str:username>/', views.user_detail, name='user_detail'),
 ]
